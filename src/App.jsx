47a3438c36c873c7b348f7cb3d9d943425aef097
labibaltasfi <labibaltasfi5@gmail.com>
2025-12-15 21:12:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>eTutionBd</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    // Component: RootLayout,
+    children: [
+       
+    ]
+  },
+]);
